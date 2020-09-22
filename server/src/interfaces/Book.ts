7d64c78ec8072c55c1a1214fe40f624dc8511307
Dayna Leal <dayna.r.leal@gmail.com>
2020-09-22 01:29:22 -0500

    added Book interface

---
+++ server/src/interfaces/Book.ts
@@ -0,0 +1,14 @@
+export interface Book {
+  title: string
+  id: string
+  author: string
+  genre: string
+  publishedYear: number
+  pageCount: number
+  type: string
+  description: string
+  rating: number
+  review: string
+}
+
+//https://www.typescriptlang.org/docs/handbook/interfaces.html
