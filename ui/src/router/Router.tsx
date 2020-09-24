bd227139c4018fee73082e1dec9e7c3555c86d8d
Dayna Leal <dayna.r.leal@gmail.com>
2020-09-24 15:03:24 -0500

    added router but all routing to /

---
+++ ui/src/router/Router.tsx
@@ -0,0 +1,23 @@
+import React, { Suspense } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+import Landing from '../views/Landing'
+import Mybooks from '../views/Mybooks'
+
+const Router = () => {
+  return(
+    <>
+      <Suspense fallback={<div>Loading Content...</div>}>
+        <main>
+          <Switch>
+            <Route path='/' component={Landing}/>
+            <Route path='/mybooks' component={Mybooks}/>
+            <Redirect from="/" to="/"/>
+          </Switch>
+        </main>
+      </Suspense>
+    </>
+  )
+}
+
+export default Router
