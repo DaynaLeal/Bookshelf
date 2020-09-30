f16d012895b01a2a8e538d9b8995755e42845b5a
Dayna Leal <dayna.r.leal@gmail.com>
2020-09-29 23:48:37 -0500

    added link to mybooks on landing page

--- ui/src/views/Landing.tsx
+++ ui/src/views/Landing.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return(
-    <div>Landing Page</div>
+    <div>
+      <h1>Landing Page</h1>
+      <Link to='/mybooks'>View My Bookshelf</Link>
+    </div>
   )
 }
 
